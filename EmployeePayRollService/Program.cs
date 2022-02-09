e7d315ffa4df0bd65d6593d124f737ee22a5f804
Akhil <akhilu5042@gmail.com>
2022-02-09 17:07:55 +0530

    Welcome to EmployeePayroll ADO.Net!

---
+++ EmployeePayRollService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeePayrollADO.Net
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to EmployeePayroll ADO.Net!");
+        }
+    }
+}
